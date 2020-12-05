09f6702155c523f49d5aa311ccee75c7fa42e4dc
manish <manishpamnani169@gmail.com>
2020-12-05 17:01:31 +0530

    Integrate: Integrate functinality for calculating the difference between new year eve & current date

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-// Get the difference between new year and today's date
-let newYear = Date.parse('01 Jan 2021 00:00:00 GMT')
-let currentDate = Date.now()
+function getDifference() {
+    // This is neccessary to get the time of indian standard (GMT + 0530)
+    let newYear = Date.parse('01 Jan 2021 00:00:00 GMT+0530')
+    console.log(new Date())
+    let date = new Date();
+    console.log(Date.parse(date.toString()))
+    let currentDate = Date.now()
+    console.log(currentDate)
 
-console.log(newYear)
-console.log(currentDate)
+    // get total seconds between the times
+    var delta = Math.abs(currentDate - newYear) / 1000;
 
+    // calculate (and subtract) whole days
+    var days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+    console.log(days)
+    console.log(delta)
 
-// The Difference
-let difference = currentDate - newYear
-console.log(Math.abs(difference))
+    // calculate (and subtract) whole hours
+    var hours = Math.floor(delta / 3600) % 24;
+    delta -= hours * 3600;
+    console.log(hours)
+    console.log(delta)
 
+    // calculate (and subtract) whole minutes
+    var minutes = Math.floor(delta / 60) % 60;
+    delta -= minutes * 60;
+    console.log(minutes)
+    console.log(delta)
 
-// Milliseconds
-console.log(parseInt(Math.abs(difference)))
+    // what's left is seconds
+    var seconds = delta % 60;
+    console.log(seconds)
 
-// Seconds
-let seconds = difference / 1000
-console.log(parseInt(Math.abs(seconds)))
+}
 
-// Minutes
-let minutes = seconds / 60;
-console.log(parseInt(Math.abs(minutes)))
-
-// Hours
-let hours = minutes / 60;
-console.log(parseInt(Math.abs(hours)))
-
-// Days
-let days = hours / 24
-console.log(parseInt(Math.abs(days)))
+getDifference()
